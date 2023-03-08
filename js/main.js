2bfee74115fcd2ee4b9844edb58a4b2f2b275d99
JustinWested <justwested@gmail.com>
2023-03-08 01:21:50 -0800

    Final solution for hero image

--- js/main.js
+++ js/main.js
@@ -1,52 +1,70 @@
-const heroImage = document.querySelector('.hero');
+/* const heroImage = document.querySelector('.hero');
 
-document.querySelector('.actors').addEventListener('mouseover', () => {
-    heroImage.style.backgroundImage = 'url(../images/hero_actor.webp)';
-  });
-document.querySelector('.production').addEventListener('mouseover', () => {
-    heroImage.style.backgroundImage = 'url(../images/hero_prod.webp)';
-  });
-document.querySelector('.photos').addEventListener('mouseover', () => {
-    heroImage.style.backgroundImage = 'url(../images/hero_photos.webp)';
-  });
-document.querySelector('.branding').addEventListener('mouseover', () => {
-    heroImage.style.backgroundImage = 'url(../images/hero_branding.webp)';
-  });
-document.querySelector('.weddings').addEventListener('mouseover', () => {
-    heroImage.style.backgroundImage = 'url(../images/hero_wedding.webp)';
-  });
-/*
+
 document.querySelector('.actors').addEventListener('mouseover', () => {
   heroImage.classList.add('heroImageActors');
 });
-document.querySelector('.actors').addEventListener('mouseout', () => {
-  heroImage.classList.remove('heroImageActors');
-});
+        document.querySelector('.actors').addEventListener('mouseout', () => {
+        heroImage.classList.remove('heroImageActors');
+        });
 
 document.querySelector('.production').addEventListener('mouseover', () => {
   heroImage.classList.add('heroImageProduction');
 });
-document.querySelector('.production').addEventListener('mouseout', () => {
-  heroImage.classList.remove('heroImageProduction');   
-});
+        document.querySelector('.production').addEventListener('mouseout', () => {
+        heroImage.classList.remove('heroImageProduction');   
+        });
 
 document.querySelector('.photos').addEventListener('mouseover', () => {
   heroImage.classList.add('heroImagePhotos');
 });
-document.querySelector('.photos').addEventListener('mouseout', () => {
-  heroImage.classList.remove('heroImagePhotos');
-});
+        document.querySelector('.photos').addEventListener('mouseout', () => {
+        heroImage.classList.remove('heroImagePhotos');
+        });
 
 document.querySelector('.branding').addEventListener('mouseover', () => {
   heroImage.classList.add('heroImageBranding');
 });
-document.querySelector('.branding').addEventListener('mouseout', () => {
-  heroImage.classList.remove('heroImageBranding');
-});
+        document.querySelector('.branding').addEventListener('mouseout', () => {
+        heroImage.classList.remove('heroImageBranding');
+        });
 
 document.querySelector('.weddings').addEventListener('mouseover', () => {
   heroImage.classList.add('heroImageWeddings');
 });
-document.querySelector('.weddings').addEventListener('mouseout', () => {
-  heroImage.classList.remove('heroImageWeddings');
-}); */
+        document.querySelector('.weddings').addEventListener('mouseout', () => {
+        heroImage.classList.remove('heroImageWeddings');
+        });  
+ */
+
+
+
+        const heroImage = document.querySelector('.hero');
+
+const actorsButton = document.querySelector('.actors');
+const productionButton = document.querySelector('.production');
+const photosButton = document.querySelector('.photos');
+const brandingButton = document.querySelector('.branding');
+const weddingsButton = document.querySelector('.weddings');
+
+actorsButton.addEventListener('mouseover', () => {
+  heroImage.style.backgroundImage = 'url(../images/hero_actor.webp)';
+});
+
+productionButton.addEventListener('mouseover', () => {
+  heroImage.style.backgroundImage = 'url(../images/hero_prod.webp)';
+});
+
+photosButton.addEventListener('mouseover', () => {
+  heroImage.style.backgroundImage = 'url(../images/hero_photos.webp)';
+});
+
+brandingButton.addEventListener('mouseover', () => {
+  heroImage.style.backgroundImage = 'url(../images/hero_branding.webp)';
+});
+
+weddingsButton.addEventListener('mouseover', () => {
+  heroImage.style.backgroundImage = 'url(../images/hero_wedding.webp)';
+});
+
+ 
