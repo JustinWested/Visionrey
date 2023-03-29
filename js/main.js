cb805bb05c6dc5810335eac28095c32e1b7f060e
JustinWested <justwested@gmail.com>
2023-03-29 01:58:24 -0700

    New body images added

--- js/main.js
+++ js/main.js
@@ -1,45 +1,4 @@
-/* const heroImage = document.querySelector('.hero');
-
-
-document.querySelector('.actors').addEventListener('mouseover', () => {
-  heroImage.classList.add('heroImageActors');
-});
-        document.querySelector('.actors').addEventListener('mouseout', () => {
-        heroImage.classList.remove('heroImageActors');
-        });
-
-document.querySelector('.production').addEventListener('mouseover', () => {
-  heroImage.classList.add('heroImageProduction');
-});
-        document.querySelector('.production').addEventListener('mouseout', () => {
-        heroImage.classList.remove('heroImageProduction');   
-        });
-
-document.querySelector('.photos').addEventListener('mouseover', () => {
-  heroImage.classList.add('heroImagePhotos');
-});
-        document.querySelector('.photos').addEventListener('mouseout', () => {
-        heroImage.classList.remove('heroImagePhotos');
-        });
-
-document.querySelector('.branding').addEventListener('mouseover', () => {
-  heroImage.classList.add('heroImageBranding');
-});
-        document.querySelector('.branding').addEventListener('mouseout', () => {
-        heroImage.classList.remove('heroImageBranding');
-        });
-
-document.querySelector('.weddings').addEventListener('mouseover', () => {
-  heroImage.classList.add('heroImageWeddings');
-});
-        document.querySelector('.weddings').addEventListener('mouseout', () => {
-        heroImage.classList.remove('heroImageWeddings');
-        });  
- */
-
-
-
-        const heroImage = document.querySelector('.hero');
+const heroImage = document.querySelector('.hero');
 
 const actorsButton = document.querySelector('.actors');
 const productionButton = document.querySelector('.production');
